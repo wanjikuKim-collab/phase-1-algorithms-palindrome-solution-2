29b1795776e7b23e91bab7d6b6db180b954ca100
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-06 00:18:11 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  //iterate from the beginning to the middle
+  for(let i=0;i<word.length/2;i++){
+    //check each letter to the corresponding letter from the end
+    const j=word.length -1-i
+    //if any letters don't match return false
+    if(word[i]!== word [j]){
+      return false
+    }}
+    return true
+  }
+
 
 /* 
   Add your pseudocode here
+  iterate from the beginning to the middle 
+    check each letter to the corresponding letter from the end
+      if any letters don't match return false
+      else return true
 */
 
 /*
